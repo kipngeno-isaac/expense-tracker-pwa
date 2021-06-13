0efde6cfd855813f2bfacf8136757c39145bd155
kipngeno-isaac <75642106+kipngeno-isaac@users.noreply.github.com>
2021-06-13 19:31:33 +0300

    feat: Added service worker

--- app.js
+++ app.js
@@ -1,67 +1,65 @@
 document.addEventListener('DOMContentLoaded', () => {
   const app = document.getElementById('app');
-  const body = document.querySelector('body');
+  const expenseCategories = document.querySelector('.expense_categories');
 
   const state = {};
 
   state.auth = false;
+  const data = [
+    {
+      name: 'Food',
+      amount: 1200
+    },
+    {
+      name: 'Entertainment',
+      amount: 3200
+    },
+    {
+      name: 'Fare',
+      amount: 1000
+    },
+    {
+      name: 'Medical',
+      amount: 11200
+    },
+  ]
 
-  if (state.auth) {
-    body.innerHTML = `<div class="login">
-    <form >
-      <div class="form-input">
-        <label>Enter Email</label>
-        <input type="text" name="email" > 
-      </div>
-      <div class="form-input">
-        <label>Enter Password</label>
-        <input type="password" name="password" > 
-      </div>
-      <button>Login</button>
-    </form>
-  </div>`;
+  let totalExpenses = 0;
+  for (let i = 0; i < data.length; i++) {
+    console.log('i :', data[i]);
+    totalExpenses += data[i].amount;
+    const expenseWrapper = document.createElement('div');
+    expenseWrapper.classList.add('expense');
 
 
+    const expenseName = document.createElement('div');
+    expenseName.classList.add('expense_name');
+    expenseName.textContent = data[i].name;
+    expenseWrapper.appendChild(expenseName);
+
+    const expenseAmount = document.createElement('div');
+    expenseAmount.classList.add('expense_amount');
+    expenseAmount.textContent = data[i].amount.toLocaleString('en-KE', { style: 'currency', currency: 'KES' });
+    expenseWrapper.appendChild(expenseAmount);
+
+    expenseCategories.appendChild(expenseWrapper);
   }
 
-  for (let i = 0; i < 20; i++) {
-    console.log('i :', i);
-    const div = document.createElement('div');
-    div.classList.add('content');
+  document.querySelector('.expense_total').textContent = totalExpenses.toLocaleString('en-KE', { style: 'currency', currency: 'KES' });
 
+  document.querySelector('.btn').addEventListener('click', (event) => {
+    event.preventDefault();
+    window.location.href = 'addExpense.html';
+  })
 
-    const h2 = document.createElement('h2');
-    h2.textContent = 'Annilation';
-    div.appendChild(h2);
-
-    const p = document.createElement('p');
-    p.textContent = 'Hello Am learning sth here';
-    div.appendChild(p);
-
-    app.appendChild(div);
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("/serviceWorker.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err))
+    })
   }
-
-
-
-
-
-  //   body.innerHTML = `<div class="register">
-  //   <form class="form">
-  //     <div class="form-input">
-  //       <label>Enter Name</label>
-  //       <input type="text" name="email" > 
-  //     </div>
-  //     <div class="form-input">
-  //       <label>Enter Email</label>
-  //       <input type="email" name="email" > 
-  //     </div>
-  //     <div class="form-input">
-  //       <label>Enter Password</label>
-  //       <input type="password" name="password" > 
-  //     </div>
-  //     <button>Login</button>
-  //   </form>
-  // </div>`;
-
+  
   console.log('content loaded');
 });
